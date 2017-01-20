52415160687b70b73f17189a7c7d0a5d711157f3
Adam Tse <mr.adam.tse@gmail.com>
2017-01-19 22:21:49 -0500

    connect root to index ejs

--- server.js
+++ server.js
@@ -2,9 +2,17 @@ const express         = require('express'),
       bodyParser      = require('body-parser'),
       mongoose        = require('mongoose'),
       app             = express(),
+      path            = require('path'),
       port            = 3000;
 
+//view engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
+//route
+app.get('/', (req, res)=>{
+  res.render('index')
+})
 
 //Setup Port
 app.listen(port, (err)=>{
