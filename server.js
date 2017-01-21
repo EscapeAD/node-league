d81831375ac751fb3fb5c54837c6b43f5bee5f3c
Adam Tse <mr.adam.tse@gmail.com>
2017-01-20 21:17:05 -0500

    +request-promises json api call

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express         = require('express'),
       bodyParser      = require('body-parser'),
       mongoose        = require('mongoose'),
+      request         = require('request-promise'),
       app             = express(),
       path            = require('path'),
       port            = 3000;
@@ -10,9 +11,35 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 //route
+
 app.get('/', (req, res)=>{
   res.render('index')
 })
+
+app.get('/summoner/:name', (req, res)=>{
+  //request-promise
+  request({
+    uri: `https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/${req.params.name}`,
+    qs: {
+      api_key: ''
+         // Use your accuweather API key here
+    },
+    json: true
+  })
+    .then((data) => {
+      res.send('index', data)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.send(err)
+    })
+
+
+
+
+
+})
+
 
 //Setup Port
 app.listen(port, (err)=>{
